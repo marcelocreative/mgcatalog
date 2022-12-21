e4e087088c8b6fc1f4b7e78d2a579cbd41518ef9
marcelocreative <marcelolnoliveira@gmail.com>
2022-12-21 08:57:14 -0300

    CategoryResource, H2 database

---
+++ backend/src/main/java/com/mgsystems/mgcatalog/resources/CategoryResource.java
@@ -0,0 +1,29 @@
+package com.mgsystems.mgcatalog.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mgsystems.mgcatalog.entities.Category;
+import com.mgsystems.mgcatalog.services.CategoryService;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryResource {
+
+		@Autowired
+		private CategoryService service;
+		
+		@GetMapping
+		public ResponseEntity<List<Category>> findAll(){
+			
+			List<Category> list = service.findAll();
+			
+			return ResponseEntity.ok(list);
+		}
+
+}
