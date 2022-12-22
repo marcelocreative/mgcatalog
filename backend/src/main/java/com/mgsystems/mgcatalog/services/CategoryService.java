5486729878476894eb2df4c12536df6c0a7e689a
marcelocreative <marcelolnoliveira@gmail.com>
2022-12-22 07:40:26 -0300

    Find Category by id

--- backend/src/main/java/com/mgsystems/mgcatalog/services/CategoryService.java
+++ backend/src/main/java/com/mgsystems/mgcatalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.mgsystems.mgcatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mgsystems.mgcatalog.dto.CategoryDTO;
 import com.mgsystems.mgcatalog.entities.Category;
@@ -16,11 +18,21 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		
 		List<Category> list = repository.findAll();
 		
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		
+		return new CategoryDTO(entity);
 	}
 
 }
