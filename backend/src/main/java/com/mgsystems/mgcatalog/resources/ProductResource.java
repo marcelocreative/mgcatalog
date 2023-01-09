c93d72e057a2b40f84032f4b220813f1ce558e78
marcelocreative <marcelolnoliveira@gmail.com>
2023-01-09 19:33:54 -0300

    Product repository, service, resource, GET ok

---
+++ backend/src/main/java/com/mgsystems/mgcatalog/resources/ProductResource.java
@@ -0,0 +1,38 @@
+package com.mgsystems.mgcatalog.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mgsystems.mgcatalog.dto.ProductDTO;
+import com.mgsystems.mgcatalog.services.ProductService;
+
+@RestController
+@RequestMapping(value= "/products")
+public class ProductResource {
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+		
+		Page<ProductDTO> list = service.findAllPaged(pageable);
+		
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
+		
+		ProductDTO dto = service.findById(id);
+		
+		return ResponseEntity.ok(dto);
+	}
+
+}
