0174ceb7daa97c8a4598cabbe9788fc19e278807
marcelocreative <marcelolnoliveira@gmail.com>
2023-12-05 08:52:24 -0300

    DTO

--- backend/src/main/java/com/mgsystems/mgcatalog/services/CategoryService.java
+++ backend/src/main/java/com/mgsystems/mgcatalog/services/CategoryService.java
@@ -1,11 +1,12 @@
 package com.mgsystems.mgcatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.mgsystems.mgcatalog.entities.Category;
+import com.mgsystems.mgcatalog.dto.CategoryDTO;
 import com.mgsystems.mgcatalog.repositories.CategoryRepository;
 
 @Service
@@ -14,9 +15,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
+	public List<CategoryDTO> findAll() {
 		
-		return repository.findAll();
+		return repository.findAll().stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
